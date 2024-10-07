e5a23a1cab75876c1b111f86a992b8b820bd6be9
wmarttala <wmarttala123@gmail.com>
2024-10-07 11:49:46 -0500

    Added name to User entity annotation and updated JoinTable column names

--- src/main/java/com/cooksys/social_media/entities/User.java
+++ src/main/java/com/cooksys/social_media/entities/User.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
@@ -19,45 +20,44 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "user_table")
 @NoArgsConstructor
 @Getter
 @Setter
 public class User {
 
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@CreationTimestamp
 	@Column(updatable = false)
 	private Timestamp joined;
-	
+
 	@Column(nullable = false)
 	private Boolean deleted;
-	
+
 	@Embedded
 	private Credentials credentials;
-	
+
 	@Embedded
 	private Profile profile;
-	
+
 	@OneToMany(mappedBy = "author")
 	private List<Tweet> tweets = new ArrayList<>();
-	
+
 	@ManyToMany
-	@JoinTable(name = "user_likes")
+	@JoinTable(name = "user_likes", joinColumns = @JoinColumn(name = "tweet_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
 	private List<Tweet> likes = new ArrayList<>();
-	
+
 	@ManyToMany
-	@JoinTable(name = "user_mentions")
+	@JoinTable(name = "user_mentions", joinColumns = @JoinColumn(name = "tweet_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
 	private List<Tweet> mentions = new ArrayList<>();
-	
+
 	@ManyToMany
-	@JoinTable(name = "followers_following")
+	@JoinTable(name = "followers_following", joinColumns = @JoinColumn(name = "follower_id"), inverseJoinColumns = @JoinColumn(name = "followed_id"))
 	private List<User> followers = new ArrayList<>();
-	
+
 	@ManyToMany(mappedBy = "followers")
 	private List<User> following = new ArrayList<>();
 
